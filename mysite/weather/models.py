305ae4fefea99b5af113135e721ae11e5cf4be63
Emily Filmer <edf7470@rit.edu>
2017-10-14 23:30:06 +0100

    added css and email address form

--- mysite/weather/models.py
+++ mysite/weather/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class Subscription(models.Model):
-    email_address = models.CharField(max_length=200)
+    email_address = models.EmailField()
     location = models.CharField(max_length=200)
--- mysite/weather/views.py
+++ mysite/weather/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import SubscriptionForm
 
 
 def index(request):
-    return render(request, "weather\index.html")
+    form = SubscriptionForm()
+    return render(request, 'weather\index.html', {'form': form})
