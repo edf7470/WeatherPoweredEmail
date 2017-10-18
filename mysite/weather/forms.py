b7d939ff2c05e2a6f9513022c3e3fe1957a6e30a
Emily Filmer <edf7470@rit.edu>
2017-10-18 03:01:05 +0100

    added forms and models for 'Sucscription'

--- mysite/weather/forms.py
+++ mysite/weather/forms.py
@@ -2,8 +2,15 @@ from django import forms
 from .models import Subscription
 
 
+# User can subscribe for Weather-Powered-Email by providing email address and location to the "Subscription Form"
 class SubscriptionForm(forms.ModelForm):
 
+    # Meta class describes the input form based on the model object 'Subscription' from models.py
     class Meta:
         model = Subscription
-        fields = ('email_address', 'location',)
+        fields = (
+            'email_address',
+            'location',
+        )
+
+
--- mysite/weather/models.py
+++ mysite/weather/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
+# Array of tuples lists the "Location" options
+TOP_100_CITIES = (
+    ("New York, NY", 'NY'),
+    ("Chicago", 'CH'),
+    ("Dallas, TX", 'DA'),
+    ("LosAngeles, CA", 'LA'),
+)
+
 
 class Subscription(models.Model):
-    email_address = models.EmailField()
-    location = models.CharField(max_length=200)
+    email_address = models.EmailField(verbose_name="Email Address")
+    location = models.CharField(max_length=20, choices=TOP_100_CITIES)
+
+
+
